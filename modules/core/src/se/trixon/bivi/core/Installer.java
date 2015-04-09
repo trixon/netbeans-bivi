20a12dab6e96bbafb4690858ead7f2fe14fe01e1
Patrik Karlsson <patrik@trixon.se>
2015-04-09 21:06:00 +0200

    Use standard tabs

--- modules/core/src/se/trixon/bivi/core/Installer.java
+++ modules/core/src/se/trixon/bivi/core/Installer.java
@@ -16,11 +16,9 @@
 package se.trixon.bivi.core;
 
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 import org.openide.modules.ModuleInstall;
 import org.openide.windows.WindowManager;
 import se.trixon.almond.Xlog;
-import se.trixon.almond.swing.NoTabsTabDisplayerUI;
 import se.trixon.bivi.core.about.AboutInitializer;
 
 public class Installer extends ModuleInstall {
@@ -29,8 +27,6 @@ public class Installer extends ModuleInstall {
     public void restored() {
         AboutInitializer.init();
         SwingUtilities.invokeLater(() -> {
-            UIManager.put("EditorTabDisplayerUI", NoTabsTabDisplayerUI.ID);
-            UIManager.put("NbMainWindow.showCustomBackground", Boolean.TRUE);
             System.setProperty("netbeans.winsys.status_line.path", "se/trixon/bivi/core/windows/statusbar/se-trixon-bivi-core-windows-statusbar-StatusBar.instance");
         });
         
